a64071d6e984bedfcbc52d4a8c5c013b128eb7a8
pipixin-525 <Yanxin_525@163.com>
2020-05-26 14:46:59 +0800

    Create Demo02variable.java

---
+++ Demo02variable.java
@@ -0,0 +1,45 @@
+public class Demo02variable{
+	public static void main(String[] args){
+		int num1;
+		num1 = 10;
+		System.out.println(num1);
+		num1 = 20;
+		System.out.println(num1);
+		
+		int num2 = 30;
+		System.out.println(num2);
+		num2 = 40;
+		System.out.println(num2);
+		
+		System.out.println("***********************");
+		
+		byte num3 = 50;
+		System.out.println(num3);
+		
+		short num4 = 60;
+		System.out.println(num4);
+		
+		long num5 = 3000000000L;
+		System.out.println(num5);
+		
+		float num6 = 2.5F;
+		System.out.println(num6);
+		
+		double num7 = 1.2;
+		System.out.println(num7);
+		
+		char zifu1 = 'A';
+		System.out.println(zifu1);
+		zifu1 = 'жа';
+		System.out.println(zifu1);
+		
+		boolean var1 = true;
+		System.out.println(var1);
+		var1 = false;
+		System.out.println(var1);
+		
+		boolean var2 = var1;
+		System.out.println(var2);
+		
+	}
+}
