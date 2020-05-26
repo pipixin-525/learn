83b73be1f2e7275da779310008bad1437fb8937f
pipixin-525 <Yanxin_525@163.com>
2020-05-26 13:23:17 +0800

    Create Demo01Const.java

---
+++ Demo01Const.java
@@ -0,0 +1,15 @@
+public class Demo01Const{
+	public static void main(String[] args){
+		//字符串常量
+		System.out.println("abc");
+		//整型常量
+		System.out.println(5);
+		//浮点数常量
+		System.out.println(1.5);
+		//字符常量
+		System.out.println('a');
+		//布尔常量
+		System.out.println(true);
+		System.out.println(false);
+	}
+}
