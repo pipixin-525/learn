e21da7c804bab779d1f5d6a4a7e33b526a31e73e
pipixin-525 <Yanxin_525@163.com>
2020-06-01 22:27:35 +0800

    Create Demo21DoWhile.java

---
+++ Demo21DoWhile.java
@@ -0,0 +1,9 @@
+public class Demo21DoWhile{
+	public static void main(String[] args){
+		int i = 1;//初始化语句
+		do{
+			System.out.println("循环体" + i);//循环体
+			i++;//步进语句
+		}while(i <= 10);//条件判断
+	}
+}
