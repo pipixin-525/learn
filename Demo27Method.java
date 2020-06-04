6848fdd418b7a1bb16a166bfd85245018389f820
pipixin-525 <Yanxin_525@163.com>
2020-06-04 22:14:07 +0800

    Create Demo27Method.java

---
+++ Demo27Method.java
@@ -0,0 +1,17 @@
+//输出一个由“*”组成的20*5的矩形
+package code002;
+
+public class Demo27Method {
+    public static void main(String[] args) {
+        printMethod();
+    }
+
+    public static void printMethod() {
+        for (int j = 0; j < 5; j++) {
+            for (int i = 0; i < 20; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
