d7e88bb3c550bf95f4d64d2a1419a5ef25518cc4
pipixin-525 <Yanxin_525@163.com>
2020-06-24 20:56:44 +0800

    Create Demo48Phone.java

---
+++ Demo48Phone.java
@@ -0,0 +1,14 @@
+package code006;
+
+public class Demo48Phone {
+    String brand;
+    String color;
+    double price;
+
+    public void call(String who){
+        System.out.println("给" + who + "打电话");
+    }
+    public void sendMessage(){
+        System.out.println("群发短信");
+    }
+}
