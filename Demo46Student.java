25fa0f9de1a77c7f6f7135234b9d60e06bbd5ec4
pipixin-525 <Yanxin_525@163.com>
2020-06-24 20:56:39 +0800

    Create Demo46Student.java

---
+++ Demo46Student.java
@@ -0,0 +1,17 @@
+package code006;
+
+public class Demo46Student {
+
+    String name;
+    int age;
+
+    public void eat(){
+        System.out.println("吃饭!");
+    }
+    public void sleep(){
+        System.out.println("睡觉！");
+    }
+    public void study(){
+        System.out.println("学习！");
+    }
+}
