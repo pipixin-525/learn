0ca194c36faf99f328752c7d67fec7b7526b1330
pipixin-525 <Yanxin_525@163.com>
2020-06-28 21:30:23 +0800

    Create Demo56UsePerson.java

---
+++ Demo56UsePerson.java
@@ -0,0 +1,11 @@
+package code006;
+
+//import code006.Demo55Person;
+public class Demo56UsePerson {
+    public static void main(String[] args) {
+        Demo55Person person = new Demo55Person();
+
+        person.name = "bbb";
+        person.sayHello("aaa");
+    }
+}
