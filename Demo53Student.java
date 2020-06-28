d227a52f56f4f9484edfffcef0338ae5bb86d134
pipixin-525 <Yanxin_525@163.com>
2020-06-28 21:30:16 +0800

    Create Demo53Student.java

---
+++ Demo53Student.java
@@ -0,0 +1,31 @@
+package code006;
+
+public class Demo53Student {
+    private String name;
+    private int age;
+    private boolean gender;
+
+    public void setName(String str) {
+        name = str;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int num) {
+        age = num;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setGender(boolean a) {
+        gender = a;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+}
