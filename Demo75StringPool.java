319a2d726abdb5532ea1f265362d3f11d8ff5467
pipixin-525 <65937242+pipixin-525@users.noreply.github.com>
2020-07-15 21:49:02 +0800

    Create Demo75StringPool.java

---
+++ Demo75StringPool.java
@@ -0,0 +1,21 @@
+package code008;
+
+public class Demo75StringPool {
+    public static void main(String[] args) {
+        String str1 = "abc";
+        String str2 = "abc";
+
+        char[] charArray = {'a', 'b', 'c'};
+        String str3 = new String(charArray);
+
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);
+
+        System.out.println("=====");
+
+        System.out.println(str1 == str2);
+        System.out.println(str1 == str3);
+        System.out.println(str2 == str3);
+    }
+}
