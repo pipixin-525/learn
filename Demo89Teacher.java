373f576c513fb2127b398f941b95c63334fd31d2
pipixin-525 <65937242+pipixin-525@users.noreply.github.com>
2020-07-21 19:51:17 +0800

    Create Demo89Teacher.java

---
+++ Demo89Teacher.java
@@ -0,0 +1,9 @@
+package code009;
+//定义了一个员工的子类：讲师
+public class Demo89Teacher extends Demo89Employee {
+    int numZi = 20;
+    int num = 200;
+    public void methodZi(){
+        System.out.println(num);
+    }
+}
