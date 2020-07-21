754b8ed38b28ed6be2ce739784dc0322a51cbf7f
pipixin-525 <65937242+pipixin-525@users.noreply.github.com>
2020-07-21 19:51:13 +0800

    Create Demo89Employee.java

---
+++ Demo89Employee.java
@@ -0,0 +1,12 @@
+package code009;
+//定义一个父类：员工
+public class Demo89Employee {
+    int numFu = 10;
+    int num = 100;
+    public void methodFu(){
+        System.out.println(num);
+    }
+    public void method(){
+        System.out.println("方法执行");
+    }
+}
