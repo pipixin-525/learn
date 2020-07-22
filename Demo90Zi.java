70d93965d067a5b7ca7421cb4726cd845ba35419
pipixin-525 <65937242+pipixin-525@users.noreply.github.com>
2020-07-22 13:53:57 +0800

    Create Demo90Zi.java

---
+++ Demo90Zi.java
@@ -0,0 +1,12 @@
+package code009;
+
+public class Demo90Zi extends Demo90Fu {
+
+    public void methodZi() {
+        System.out.println("子类方法执行！");
+    }
+
+    public void method() {
+        System.out.println("子类重名方法执行！");
+    }
+}
