1b4f938b011bd6467235462acfe3dcf523b37666
pipixin-525 <65937242+pipixin-525@users.noreply.github.com>
2020-07-22 13:54:00 +0800

    Create Demo90ExtendsMethod.java

---
+++ Demo90ExtendsMethod.java
@@ -0,0 +1,13 @@
+package code009;
+
+public class Demo90ExtendsMethod {
+
+    public static void main(String[] args) {
+        Demo90Zi zi = new Demo90Zi();
+
+        zi.methodFu();
+        zi.methodZi();
+
+        zi.method();
+    }
+}
