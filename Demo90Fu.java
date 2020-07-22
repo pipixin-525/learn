fa0c46463162494e3611b49cece1e66bbc74ebeb
pipixin-525 <65937242+pipixin-525@users.noreply.github.com>
2020-07-22 13:53:55 +0800

    Create Demo90Fu.java

---
+++ Demo90Fu.java
@@ -0,0 +1,12 @@
+package code009;
+
+public class Demo90Fu {
+
+    public void methodFu() {
+        System.out.println("父类方法执行！");
+    }
+
+    public void method() {
+        System.out.println("父类重名方法执行！");
+    }
+}
