2dcbb0b125837b8bf200d7136dcc404d63d4a215
pipixin-525 <65937242+pipixin-525@users.noreply.github.com>
2020-07-26 14:16:59 +0800

    Create Demo94MyInterface.java

---
+++ Demo94MyInterface.java
@@ -0,0 +1,12 @@
+package code011;
+
+public interface Demo94MyInterface {
+
+    public abstract void methodAbs1();
+
+    public void methodAbs2();
+
+    abstract void methodAbs3();
+
+    void methodAbs4();
+}
