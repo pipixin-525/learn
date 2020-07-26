7fea0d6a3b7c16dfede7c71b967491be8729198e
pipixin-525 <65937242+pipixin-525@users.noreply.github.com>
2020-07-26 14:16:57 +0800

    Create Demo94Interface.java

---
+++ Demo94Interface.java
@@ -0,0 +1,14 @@
+package code011;
+
+public class Demo94Interface {
+
+    public static void main(String[] args) {
+        Demo94MyInterfaceImpl impl = new Demo94MyInterfaceImpl();
+
+        impl.methodAbs1();
+        impl.methodAbs2();
+        impl.methodAbs3();
+        impl.methodAbs4();
+    }
+
+}
