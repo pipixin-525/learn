42f48211f171b652c91109e4e39325d14943cafe
pipixin-525 <65937242+pipixin-525@users.noreply.github.com>
2020-07-27 11:48:46 +0800

    Create Demo96InterfacePrivate1.java

---
+++ Demo96InterfacePrivate1.java
@@ -0,0 +1,20 @@
+package code011;
+
+public interface Demo96InterfacePrivate1 {
+
+    public default void methodDefault1() {
+        System.out.println("默认方法1");
+        methodCommon();
+    }
+
+    public default void methodDefault2() {
+        System.out.println("默认方法2");
+        methodCommon();
+    }
+
+    private void methodCommon() {
+        System.out.println("AAA");
+        System.out.println("BBB");
+        System.out.println("CCC");
+    }
+}
