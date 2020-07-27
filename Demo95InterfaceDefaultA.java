a4c106fc9380aa7634de797126b10197e2a399f4
pipixin-525 <65937242+pipixin-525@users.noreply.github.com>
2020-07-27 11:48:32 +0800

    Create Demo95InterfaceDefaultA.java

---
+++ Demo95InterfaceDefaultA.java
@@ -0,0 +1,8 @@
+package code011;
+
+public class Demo95InterfaceDefaultA implements Demo95InterfaceDefault {
+    @Override
+    public void methodAbs() {
+        System.out.println("实现了抽象方法A");
+    }
+}
