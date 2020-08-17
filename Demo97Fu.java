bd024e811e4acdd9627616c58263e3505aab621d
pipixin-525 <65937242+pipixin-525@users.noreply.github.com>
2020-08-17 18:33:39 +0800

    Create Demo97Fu.java

---
+++ Demo97Fu.java
@@ -0,0 +1,18 @@
+package code012;
+
+
+public class Demo97Fu {
+    public void method() {
+        System.out.println("父类方法");
+    }
+
+    public void methodFu() {
+        System.out.println("父类特有方法");
+    }
+
+    int num = 10;
+
+    public void showNum() {
+        System.out.println(num);
+    }
+}
