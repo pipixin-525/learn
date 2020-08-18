f727bcabb344c8aaf892abc544f0b607ea1aea11
pipixin-525 <65937242+pipixin-525@users.noreply.github.com>
2020-08-18 20:06:15 +0800

    Create Demo98Dog.java

---
+++ Demo98Dog.java
@@ -0,0 +1,12 @@
+package code012;
+
+public class Demo98Dog extends Demo98Animal {
+    @Override
+    void eat() {
+        System.out.println("狗吃骨头");
+    }
+
+    public void watchHouse() {
+        System.out.println("够看家");
+    }
+}
