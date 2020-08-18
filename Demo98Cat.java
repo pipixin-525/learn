c61c80dc51d63005f0f3681799e0671d44383571
pipixin-525 <65937242+pipixin-525@users.noreply.github.com>
2020-08-18 20:06:13 +0800

    Create Demo98Cat.java

---
+++ Demo98Cat.java
@@ -0,0 +1,12 @@
+package code012;
+
+public class Demo98Cat extends Demo98Animal {
+    @Override
+    void eat() {
+        System.out.println("猫吃鱼");
+    }
+
+    public void catchMouse() {
+        System.out.println("猫捉老鼠");
+    }
+}
