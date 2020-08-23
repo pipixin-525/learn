d58477dd32385db3e2d4a88c52cea0131140605b
pipixin-525 <65937242+pipixin-525@users.noreply.github.com>
2020-08-23 21:33:59 +0800

    Create Demo102Main.java

---
+++ Demo102Main.java
@@ -0,0 +1,9 @@
+package code013;
+
+public class Demo102Main {
+
+    public static void main(String[] args) {
+        Demo102Outer method = new Demo102Outer();
+        method.methodOuter();
+    }
+}
