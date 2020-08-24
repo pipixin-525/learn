8dbddb42a246fc27d0330cafeb9d31d98c9bd078
pipixin-525 <65937242+pipixin-525@users.noreply.github.com>
2020-08-24 14:19:31 +0800

    Create Demo104Weapon.java

---
+++ Demo104Weapon.java
@@ -0,0 +1,21 @@
+package code014;
+
+public class Demo104Weapon {
+
+    private String Weapon;
+
+    public Demo104Weapon() {
+    }
+
+    public Demo104Weapon(String weapon) {
+        Weapon = weapon;
+    }
+
+    public String getWeapon() {
+        return Weapon;
+    }
+
+    public void setWeapon(String weapon) {
+        Weapon = weapon;
+    }
+}
