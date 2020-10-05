900dfc8ec970de3c0fd9f4eb72e83e0a41c32022
pipixin-525 <65937242+pipixin-525@users.noreply.github.com>
2020-10-05 12:16:50 +0800

    Create Demo131GenericMethod.java

---
+++ Demo131GenericMethod.java
@@ -0,0 +1,11 @@
+package code016;
+
+public class Demo131GenericMethod {
+    public <M> void method1(M m) {
+        System.out.println(m);
+    }
+
+    public static <N> void method2(N n) {
+        System.out.println(n);
+    }
+}
