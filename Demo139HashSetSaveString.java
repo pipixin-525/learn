9e48ab69ee1a3fb47b6f04a9244ada757f1838a4
pipixin-525 <65937242+pipixin-525@users.noreply.github.com>
2020-10-11 20:06:05 +0800

    Create Demo139HashSetSaveString.java

---
+++ Demo139HashSetSaveString.java
@@ -0,0 +1,19 @@
+package code017;
+
+import java.util.HashSet;
+
+public class Demo139HashSetSaveString {
+    public static void main(String[] args) {
+
+        HashSet<String> set = new HashSet<>();
+        String s1 = new String("abc");
+        String s2 = new String("abc");
+        set.add(s1);
+        set.add(s2);
+        set.add("重地");
+        set.add("通话");
+        set.add("abc");
+        System.out.println(set);
+
+    }
+}
