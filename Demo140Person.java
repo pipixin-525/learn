b92aca5d3f81ac6e23b6416c3f4ac35ab7cfcd39
pipixin-525 <65937242+pipixin-525@users.noreply.github.com>
2020-10-12 14:35:08 +0800

    Create Demo140Person.java

---
+++ Demo140Person.java
@@ -0,0 +1,55 @@
+package code017;
+
+import java.util.Objects;
+
+public class Demo140Person {
+
+    private String name;
+    private int age;
+
+    public Demo140Person() {
+    }
+
+    public Demo140Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Demo140Person that = (Demo140Person) o;
+        return age == that.age &&
+                Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Demo140Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
